1e068ed59e585aa3476c461d8552f92b48f93fff
eeloo1118 <79968287+eeloo1118@users.noreply.github.com>
2021-08-22 22:04:50 +0800

    Add files via upload

---
+++ src/main/java/com/qf/servcice/BwcarApplication.java
@@ -0,0 +1,13 @@
+package com.qf.servcice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwcarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BwcarApplication.class, args);
+    }
+
+}
